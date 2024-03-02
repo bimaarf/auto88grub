dc6a375f97efaaeae6f60bc60a3628f01c992a02
bimaarf <bimaarifa5@gmail.com>
2024-03-02 14:01:13 +0700

    --cleaning-code

---
+++ frontend/lib/Pages/Components/Data/Master/Model/BrandPage.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:frontend/Model/Services/MasterData/fetchBrand.dart';
+import 'package:frontend/Pages/Components/Home/Content/Brand/Context/__BrandList.dart';
+import 'package:frontend/Pages/Components/Home/Content/Brand/Context/__BrandStore.dart';
+import 'package:frontend/Pages/Components/Home/Content/Brand/Context/__BrandUpdate.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class BrandPage extends StatefulWidget {
+  @override
+  _BrandPageState createState() => _BrandPageState();
+}
+
+class _BrandPageState extends State<BrandPage> {
+  List<Map<String, dynamic>> brands = [];
+  bool isLoading = false;
+  late String baseUrl;
+
+  @override
+  void initState() {
+    super.initState();
+    initializeBaseUrl();
+  }
+
+  Future<void> initializeBaseUrl() async {
+    await dotenv.load();
+    baseUrl = dotenv.env['BASE_URL']!;
+    await fetchBrand(); // Await fetchBrand
+  }
+
+  Future<String> getTokenFromStorage() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString('token') ?? '';
+  }
+
+  Future<void> fetchBrand() async {
+    try {
+      setState(() {
+        isLoading = true;
+      });
+
+      brands = await ServiceBrand.fetchBrands(baseUrl);
+
+      setState(() {
+        isLoading = false;
+      });
+    } catch (e) {
+      print('Error fetching brand data: $e');
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  void showUpdatePage(Map<String, dynamic> brand) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => UpdateBrandPage(
+          brandId: brand['id']?.toString() ?? '',
+          name: brand['name'] ?? '',
+          onUpdate: () {
+            fetchBrand();
+          },
+          fetchNewData: fetchBrand,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Brands'),
+        backgroundColor: Colors.black,
+      ),
+      body: RefreshIndicator(
+        color: Colors.white,
+        onRefresh: fetchBrand,
+        child: isLoading
+            ? const Center(
+                child: CircularProgressIndicator(
+                  color: Colors.white,
+                ),
+              )
+            : BrandList(
+                brands: brands,
+                onUpdate: (brand) {
+                  showUpdatePage(brand);
+                },
+              ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => AddBrandPage(),
+            ),
+          ).then((value) {
+            if (value == true) {
+              fetchBrand();
+            }
+          });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.white,
+      ),
+    );
+  }
+}
---
+++ frontend/lib/Pages/Components/Data/Master/Model/Context/__BrandUpdate.dart
@@ -0,0 +1,117 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+class UpdateBrandPage extends StatefulWidget {
+  final String brandId;
+  final String name;
+  final Function() onUpdate;
+  final Function() fetchNewData;
+
+  UpdateBrandPage({
+    required this.brandId,
+    required this.name,
+    required this.onUpdate,
+    required this.fetchNewData,
+  });
+
+  @override
+  _UpdateBrandPageState createState() => _UpdateBrandPageState();
+}
+
+class _UpdateBrandPageState extends State<UpdateBrandPage> {
+  TextEditingController _nameController = TextEditingController();
+  String _token = '';
+  @override
+  void initState() {
+    super.initState();
+    _nameController.text = widget.name;
+    _loadToken();
+  }
+
+  Future<void> _loadToken() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _token = prefs.getString('token') ?? '';
+    });
+  }
+
+  Future<void> updateBrand() async {
+    try {
+      String baseUrl = dotenv.env['BASE_URL']!;
+      String token = 'Bearer $_token'; // Add your token retrieval logic here
+
+      Map<String, String> headers = {
+        'Authorization': 'Bearer $token',
+        'Content-Type': 'application/json',
+      };
+
+      Map<String, dynamic> data = {
+        'name': _nameController.text,
+      };
+
+      final response = await http.post(
+        Uri.parse('$baseUrl/api/brand/update/${widget.brandId}'),
+        headers: headers,
+        body: jsonEncode(data),
+      );
+
+      if (response.statusCode == 200) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Brand updated successfully'),
+            duration: Duration(seconds: 2),
+          ),
+        );
+
+        // Call the function to fetch new data
+
+        widget.onUpdate();
+        Navigator.pop(context, true);
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+            content: Text('Brand updated err'),
+            duration: Duration(seconds: 2),
+          ),
+        );
+        print('Failed to update brand: ${response.statusCode}');
+      }
+    } catch (e) {
+      // Error updating brand
+      print('Error updating brand: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Update brand'),
+        backgroundColor: Colors.black,
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextFormField(
+              controller: _nameController,
+              decoration: const InputDecoration(labelText: 'Name'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Call the updateBrand function when the button is pressed
+                updateBrand();
+              },
+              child: const Text('Update'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
