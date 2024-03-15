dd4fe4c525bcf4888610eef281f997a61bdcade1
bimaarf <bimaarifa5@gmail.com>
2024-03-15 11:04:19 +0700

    backend: -car-promo

--- backend/app/Http/Controllers/API/Pages/CarPromoController.php
+++ backend/app/Http/Controllers/API/Pages/CarPromoController.php
@@ -24,7 +24,7 @@ class CarPromoController extends Controller
             ]);
 
         foreach ($input as $key => $value) {
-            if ($key === 'perPage' || $key === 'price' || empty($value)) {
+            if ($key === 'perPage' || $key === 'page' || $key === 'price' || empty($value)) {
                 continue;
             }
 
@@ -52,16 +52,18 @@ class CarPromoController extends Controller
     public function showPromo(Request $request)
     {
         $page = $request->query('page', 1);
-        $perPage = $request->query('perPage', 10);
+        $perPage = $request->query('perPage', 6);
         $getData = Promo::with('car')
             ->join('cars', 'cars.id', 'car_promos.car_id')
-            ->where('car_promos.is_pinned', 1)
+            // ->where('car_promos.is_pinned', 1)
             ->with([
                 'car.documents', 'car.officials', 'car.originals', 'car.outdoors',
                 'car.location', 'car.brand', 'car.model', 'car.type', 'car.cylinder', 'car.transmission', 'car.series',
                 'car.gear', 'car.fuel', 'car.color', 'car.row', 'car.year'
             ])
-            ->paginate($perPage, ['car_promos.*', 'cars.slug', 'cars.description'], 'page', $page);
+            ->select(['car_promos.*', 'cars.slug', 'cars.description'])
+            ->orderBy('id', 'DESC')
+            ->paginate($perPage, ['*'], 'page', $page);
 
         $getData->transform(function ($promo) {
             $promo->title = $promo->slug;
